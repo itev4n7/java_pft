5371c41366123fe7703148a0ee735d6064666739
Aleksey <loha.malyota@gmail.com>
2019-06-25 10:00:03 +0300

    refactor GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -12,39 +12,63 @@ import java.util.concurrent.TimeUnit;
 import static org.testng.Assert.fail;
 
 public class GroupCreationTests {
-   private WebDriver driver;
-   private String baseUrl;
-   private boolean acceptNextAlert = true;
-   private StringBuffer verificationErrors = new StringBuffer();
+   WebDriver driver;
+   String baseUrl;
+   boolean acceptNextAlert = true;
+   StringBuffer verificationErrors = new StringBuffer();
 
    @BeforeClass(alwaysRun = true)
    public void setUp() throws Exception {
       driver = new ChromeDriver();
       baseUrl = "https://www.katalon.com/";
       driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+      driver.get("http://localhost:8080/group.php");
+      login("admin", "admin");
+   }
+
+   private void login(String userName, String password) {
+      driver.findElement(By.name("user")).clear();
+      driver.findElement(By.name("user")).sendKeys(userName);
+      driver.findElement(By.name("pass")).clear();
+      driver.findElement(By.name("pass")).sendKeys(password);
+      driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
    }
 
    @Test
-   public void testUntitledTestCase() throws Exception {
-      driver.get("http://localhost:8080/group.php");
-      driver.findElement(By.name("user")).clear();
-      driver.findElement(By.name("user")).sendKeys("admin");
-      driver.findElement(By.name("pass")).clear();
-      driver.findElement(By.name("pass")).sendKeys("admin");
-      driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
-      driver.findElement(By.linkText("groups")).click();
-      driver.findElement(By.name("new")).click();
+   public void testGroupCreation() throws Exception {
+      goToGroupPage();
+      initGroupCreation();
+      fillCroupForm(new GroupData("test1", "test2", "test3"));
+      submitGroupCreation();
+      returnToGroupPage();
+   }
+
+   private void returnToGroupPage() {
+      driver.findElement(By.linkText("group page")).click();
+   }
+
+   private void submitGroupCreation() {
+      driver.findElement(By.name("submit")).click();
+   }
+
+   private void fillCroupForm(GroupData groupData) {
       driver.findElement(By.name("group_name")).click();
       driver.findElement(By.name("group_name")).clear();
-      driver.findElement(By.name("group_name")).sendKeys("test1");
+      driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
       driver.findElement(By.name("group_header")).click();
       driver.findElement(By.name("group_header")).clear();
-      driver.findElement(By.name("group_header")).sendKeys("test2");
+      driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
       driver.findElement(By.name("group_footer")).click();
       driver.findElement(By.name("group_footer")).clear();
-      driver.findElement(By.name("group_footer")).sendKeys("test3");
-      driver.findElement(By.name("submit")).click();
-      driver.findElement(By.linkText("group page")).click();
+      driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+   }
+
+   private void initGroupCreation() {
+      driver.findElement(By.name("new")).click();
+   }
+
+   private void goToGroupPage() {
+      driver.findElement(By.linkText("groups")).click();
    }
 
    @AfterClass(alwaysRun = true)
