c27c1d89538d084a4b2b729531bfc55cdb6218b5
Aleksey <loha.malyota@gmail.com>
2019-07-03 18:51:08 +0300

    add item search by index in groups

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -13,7 +13,7 @@ public class GroupModificationTests extends TestBase {
       if (!app.getGroupHelper().isThereGroup()) {
          app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
       }
-      app.getGroupHelper().selectGroup();
+      app.getGroupHelper().selectGroup(before-1);
       app.getGroupHelper().initGroupModification();
       app.getGroupHelper().fillCroupForm(new GroupData("test1", "test2", "test3"));
       app.getGroupHelper().submitGroupModification();
