bff0902956dae49accb3a736c307ed7b328edda7
Aleksey <loha.malyota@gmail.com>
2019-07-03 18:19:00 +0300

    create new test with collection

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+   public static void main(String[] args) {
+      String[] langs = {};
+      List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+      for (String l : languages) {
+         System.out.println(l);
+      }
+   }
+}
