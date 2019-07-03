7109b346657c9df45282baf58dc1021734bb36a4
Aleksey <loha.malyota@gmail.com>
2019-07-03 19:45:52 +0300

    changed the number of groups to lists

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -2,7 +2,11 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupHelper extends HelperBase {
 
@@ -57,5 +61,18 @@ public class GroupHelper extends HelperBase {
 
    public int getGroupCount() {
       return driver.findElements(By.name("selected[]")).size();
+   }
+
+   public List<GroupData> getGroupList() {
+      List<GroupData> groups = new ArrayList<>();
+      List<WebElement> elements = driver.findElements(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Groups'])[1]/following::form[1]"));
+      for (WebElement element : elements) {
+         String[] arrNameGroups = element.getText().split("\n");
+         for (String name : arrNameGroups) {
+            GroupData group = new GroupData(name, null, null);
+            groups.add(group);
+         }
+      }
+      return groups;
    }
 }
