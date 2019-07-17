8136b1f75f3577b18512d9505aef033b2392812a
Aleksey <loha.malyota@gmail.com>
2019-07-17 15:19:53 +0300

    description of the test bench configuration

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,11 +6,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.fail;
 
 public class ApplicationManager {
+   private final Properties properties;
    WebDriver driver;
    private ContactsHelper contactsHelper;
    private SessionHelper sessionHelper;
@@ -21,14 +26,17 @@ public class ApplicationManager {
 
    public ApplicationManager(String browser) {
       this.browser = browser;
+      properties = new Properties();
    }
 
-   public void init() {
+   public void init() throws IOException {
+      String target = System.getProperty("target", "local");
+      properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
       selectionWebDriver();
       driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-      driver.get("http://localhost:8080/");
+      driver.get(properties.getProperty("web.baseUrl"));
       connectionWebDriver();
-      sessionHelper.login("admin", "admin");
+      sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
    }
 
    private void connectionWebDriver() {
