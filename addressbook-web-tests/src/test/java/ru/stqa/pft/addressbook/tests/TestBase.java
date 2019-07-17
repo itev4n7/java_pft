b818a037fd5a0f14f31b61bb785be2628cf83795
Aleksey <loha.malyota@gmail.com>
2019-07-17 17:50:16 +0300

    add logging in project

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,11 +1,19 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
+   Logger logger = LoggerFactory.getLogger(TestBase.class);
 
    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
@@ -14,9 +22,19 @@ public class TestBase {
       app.init();
    }
 
-   @AfterSuite
+   @AfterSuite(alwaysRun = true)
    public void tearDown() throws Exception {
       app.stop();
    }
 
+   @BeforeMethod(alwaysRun = true)
+   public void logTestStart(Method method, Object[] parameter) {
+      logger.info("Star test " + method.getName() + " with parameters " + Arrays.asList(parameter));
+   }
+
+   @AfterMethod(alwaysRun = true)
+   public void logTestStop(Method method, Object[] parameter) {
+      logger.info("Stop test " + method.getName() + " with parameters " + Arrays.asList(parameter));
+
+   }
 }
