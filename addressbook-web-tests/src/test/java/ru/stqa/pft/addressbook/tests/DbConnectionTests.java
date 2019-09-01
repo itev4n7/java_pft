8e14ddf27f7fe923b605ed64b4bc120e00e3a110
Aleksey <loha.malyota@gmail.com>
2019-09-01 20:50:58 +0300

    replace HBconnect

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTests.java
@@ -13,7 +13,7 @@ public class DbConnectionTests {
    public void testDbConnection() {
       Connection conn = null;
       try {
-         conn = DriverManager.getConnection("jdbc:mysql://localhost/addressbook?user=root&password=");
+         conn = DriverManager.getConnection("jdbc:mysql://localhost/addressbook?user=root&password=root&serverTimezone=UTC");
          Statement st = conn.createStatement();
          ResultSet resultSet = st.executeQuery("select group_id,group_name,group_header,group_footer from group_list");
          Groups groups = new Groups();
